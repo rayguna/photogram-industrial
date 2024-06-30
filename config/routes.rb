3d0232fd38eafc1acba61e6aa4c050176aead1a1
Ray Gunawidjaja <33289654+rayguna@users.noreply.github.com>
2024-06-30 03:39:38 +0000

    generated users with devise

--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,11 @@ end
 ### B. Create a github branch
 
 1. In the terminal, type: `git checkout -b rg-create-database`. 
-2. Commit changes to git.
+2. Commit changes to git. Publish the branch.
+
+### C. Create a users table
+
+1. Type in the terminal: `rails g devise user username private:boolean likes_count:integer comments_count:integer`.
+2. Commit changes with `git add -A`.
+
+3. Then: `git commit -m "generated users with devise`. 
