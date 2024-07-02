8990835a1d09174073b7ec2fee83ef04f87fda15
Ray Gunawidjaja <33289654+rayguna@users.noreply.github.com>
2024-07-02 22:06:14 +0000

    fixed a typo within models/user.rb.

--- app/models/user.rb
+++ app/models/user.rb
@@ -34,7 +34,7 @@ class User < ApplicationRecord
   has_many :sent_follow_requests, foreign_key: :sender_id, class_name: "FollowRequest"
   has_many :received_follow_requests, foreign_key: :recipient_id, class_name: "FollowRequest"
 
-  has_many :likes, foreign_key: :
+  has_many :likes, foreign_key: :fan_id
   
   has_many :own_photos, foreign_key: :owner_id, class_name: "Photo"
 end
