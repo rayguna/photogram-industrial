4cd489dad76f41840649e6034d010a0a41153e3a
Ray Gunawidjaja <33289654+rayguna@users.noreply.github.com>
2024-07-05 22:04:35 +0000

    Made feed tab functional.

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -69,6 +69,10 @@ class PhotosController < ApplicationController
 
   end
 
+  def feed
+    
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_photo
